061a01b2c3ca6ecc4db0d2a2f8ce3d2c4a76d3fb
Nikhil <nikhild1102@gmail.com>
2019-08-16 19:13:39 +0530

    Changed mysql to postgres for heroku

--- config/database.js
+++ config/database.js
@@ -8,30 +8,11 @@ module.exports = {
     host: config.get('db.host'),
     dialect: 'mysql'
   },
-  test: {
-    username: config.get('db.user'),
-    password: config.get('db.password'),
-    database: config.get('db.name'),
-    host: config.get('db.host'),
-    dialect: 'mysql'
-  },
-  staging: {
-    username: config.get('db.user'),
-    password: config.get('db.password'),
-    database: config.get('db.name'),
-    host: config.get('db.host'),
-    dialect: 'mysql'
-  },
   production: {
     username: config.get('db.user'),
     password: config.get('db.password'),
     database: config.get('db.name'),
     host: config.get('db.host'),
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        // ca: fs.readFileSync(__dirname + '/mysql-ca-master.crt')
-      }
-    }
+    dialect: 'postgres'
   }
 }
